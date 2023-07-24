4e78660213aeb85f50de8ffa8ec4a42d60a16a97
balzoertli <135339123+balzoertli@users.noreply.github.com>
2023-07-24 12:24:09 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,9 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter, Retry
+
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
 
 url = "https://www.efk.admin.ch/de/politikfinanzierung/aufgaben-der-efk/kontrollen.html"
-entries = []
-page = requests.get(url)
+response = session.get(url)
+
 soup = BeautifulSoup(page.content, "lxml")
 elements = soup.find(id="collapse4")
 print(elements.text.strip())
+
